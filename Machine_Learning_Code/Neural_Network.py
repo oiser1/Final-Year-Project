fc5970ab0021a28ad6c7579c755b2c59370fcae3
oiser1 <oisinat75@gmail.com>
2019-04-09 23:20:27 +0100

    Updated Neural_Network file with a new data handler function to read in data for training the machine learning algorithm.

--- Machine_Learning_Code/Neural_Network.py
+++ Machine_Learning_Code/Neural_Network.py
@@ -1,4 +1,41 @@
 import tensorflow as tf
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
 
-print(21)
+leftWhisk = []
+rightWhisk = []
+
+def dataHandler(leftWhisk, rightWhisk):
+    #while (rowFlag == 
+    with open('TrainingData.csv', mode='r') as TrainingData:
+            TrainingDataReader = csv.reader(TrainingData, delimiter=',')
+            for row in TrainingDataReader:
+                row[0] = int(row[0])
+                row[1] = int(row[1])
+                leftWhisk.append(row[0])
+                rightWhisk.append(row[1])
+
+            leftWhisk = np.asarray(leftWhisk)
+            rightWhisk = np.asarray(rightWhisk)
+            #print(leftWhisk)
+            plt.plot(leftWhisk)
+            plt.ylabel('Range')
+            plt.xlabel('Samples')
+            plt.show()
+            #print("\n")
+            #print(rightWhisk)
+            plt.plot(rightWhisk)
+            plt.ylabel('Range')
+            plt.xlabel('Samples')
+            plt.show()
+def main():
+    dataHandler(leftWhisk, rightWhisk)
+    while True:
+        pass
+
+
+
+if __name__== "__main__":
+    main()
 
